56657371d4c4b55c26b9ede36f071f14ca5e42db
cristian nunez <crisn3682@gmail.com>
2024-03-03 19:18:43 -0300

    hice cambios en el proyecto

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 class ProductManager {
     constructor() {
+
+        const fs = require ("fs");
         this.products = [];
         this.codeId = 0;
     }
-    addProduct(title, description, price, thumbnail, code, stock) {
+    addProduct (title, description, price, thumbnail, code, stock) {
         if (!title || !description || !price || !thumbnail || !code || !stock) {
             console.log("Todos los campos son obligatorios");
         }
@@ -29,30 +31,29 @@ class ProductManager {
 
     getProductById(id) {
         const product = this.products.find(product => product.id === id);
+
         if (product) {
             return product;
         } else {
             console.log("Producto no encontrado");
         }
     }
-    updateProduct(id, updatedFields) {
+
+    updateProduct(id, updatedProduct) {
         const products = this.getProducts();
-        const index = products.findIndex(p => p.id === id);
-        if (index === -1) {
-        throw new Error('Producto no encontrado');
+        const index = products.findIndex(product => product.id === id);
+        if (index !== -1) {
+            products[index] = { ...updatedProduct, id };
+            fs.writeFileSync(this.path, JSON.stringify(products, null, 2));
         }
-        products[index] = { ...products[index], ...updatedFields };
-        (this.path, JSON.stringify(products, null, 2));
     }
-    
+
     deleteProduct(id) {
         const products = this.getProducts();
-        const filteredProducts = products.filter(p => p.id !== id);
-        if (filteredProducts.length === products.length) {
-        throw new Error('Producto no encontrado');
-        }
-        (this.path, JSON.stringify(filteredProducts, null, 2));
+        const filteredProducts = products.filter(product => product.id !== id);
+        fs.writeFileSync(this.path, JSON.stringify(filteredProducts, null, 2));
     }
+
 }
 
 // Ejemplo de uso
@@ -65,11 +66,4 @@ console.log(productManager.getProducts());
 
 console.log(productManager.getProductById(1));
 console.log(productManager.getProductById(3));
-productManager.updateProduct(1, { price: 0 });
-const updatedProduct = productManager.getProductById(1);
-console.log(updatedProduct);
-
-productManager.deleteProduct(1);
-const deleteProducts = productManager.getProducts();
-console.log(deleteProducts);
-
+console.log(productManager.deleteProduct(2));
